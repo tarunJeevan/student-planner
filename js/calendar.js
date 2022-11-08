6f9a87a29267ca41eb4f875f2147bcabcb273169
Maxwell Maitland <mmdawg1@aol.com>
2022-11-07 19:10:08 -0500

    completed and delete event working

--- js/calendar.js
+++ js/calendar.js
@@ -1,12 +1,12 @@
-var Calendar;
-var Draggable = FullCalendar.Draggable;
-var numClick = 0;
+var Calendar
+var Draggable = FullCalendar.Draggable
+var numClick = 0
 
 function init() {
     document.addEventListener('DOMContentLoaded',
         function () {
-            var containerEl = document.getElementById('external-events');
-            var calendarEl = document.getElementById('calendar');
+            var containerEl = document.getElementById('external-events')
+            var calendarEl = document.getElementById('calendar')
 
             // initialize the external events
 
@@ -20,9 +20,9 @@ function init() {
                             completed: false
                         },
                         classNames: ["calendarevent"]
-                    };
+                    }
                 }
-            });
+            })
 
             //initialize calendar
             Calendar = new FullCalendar.Calendar(calendarEl, {
@@ -37,73 +37,104 @@ function init() {
 
                 },
                 eventClick: function (info) {
-                    console.log(info.jsEvent);
-                    info.jsEvent.preventDefault();
-                    openCalendarMenu(info);
+                    info.jsEvent.preventDefault()
+                    openCalendarMenu(info)
                 }
-            });
+            })
 
-            Calendar.render();
-        });
+            Calendar.render()
+        })
 }
 
 function appendEvent() {
-    var eventDiv = document.getElementById("external-events");
-    eventDiv.append(createNewEventDiv());
-    return false;
+    var eventDiv = document.getElementById("external-events")
+    eventDiv.append(createNewEventDiv())
+    return false
 }
 
 function createNewEventDiv() {
-    var outerDiv = document.createElement('div');
-    var innerDiv = document.createElement('div');
+    var outerDiv = document.createElement('div')
+    var innerDiv = document.createElement('div')
 
 
-    innerDiv.className = "fc-event-main";
+    innerDiv.className = "fc-event-main"
     innerDiv.id = "eventelement"
-    innerDiv.append(document.createTextNode("My event"));
-    outerDiv.className = "fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event";
+    innerDiv.append(document.createTextNode("My event"))
+    outerDiv.className = "fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event"
 
-    outerDiv.append(innerDiv);
-    return outerDiv;
+    outerDiv.append(innerDiv)
+    return outerDiv
 }
 
 function openCalendarMenu(info) {
-    element = document.elementFromPoint(info.jsEvent.pageX, info.jsEvent.pageY);
-    console.log(element.classList);
-    if (!element.classList.contains("menuEnabled")) {
-        element.classList.add("menuEnabled");
+    element = document.elementFromPoint(info.jsEvent.pageX, info.jsEvent.pageY)
 
-        let main = document.createElement('div');
-        let pdel = document.createElement('div');
-        let pcom = document.createElement('div');
+    if (element.classList.contains("fc-event-title")) {
+        element = element.parentElement
+    }
 
-        main.classList.add("contextMenu");
+    if (!element.classList.contains("menuEnabled") && !element.classList.contains("menuItem")) {
+
+        element.classList.add("menuEnabled")
+
+        let main = document.createElement('div')
+        let pcom = document.createElement('div')
+        let pdel = document.createElement('div')
+        let canc = document.createElement('div')
+
+        main.classList.add("contextMenu")
+        pdel.classList.add("menuItem")
+        pcom.classList.add("menuItem")
+        canc.classList.add("menuItems")
+        canc.classList.add("menuItem-close")
         
-        pdel.innerText = "Delete";
-        pdel.addEventListener("onclick", function(ev){
-            deleteEvent(info)
-        });
-        pdel.classList.add("menuEnabled");
-
+        pdel.innerText = "Delete"
         pcom.innerText = "Complete"
-        pcom.addEventListener("onclick", completeEvent(info));
-        pcom.classList.add("menuEnabled");
+        canc.innerText = "Close"
 
-        main.append(pdel);
-        main.append(pcom);
-        element.append(main);
+        pdel.addEventListener('click', function () {
+            deleteEvent(info, element)
+            removeEventMenu(main)
+        })
+
+        pcom.addEventListener('click', function () {
+            completeEvent(info, element)
+            removeEventMenu(main)
+        })
+
+        canc.addEventListener('click', function () {
+            removeEventMenu(main)
+        })
+
+        main.append(pcom)
+        main.append(pdel)
+        main.append(canc)
+
+        element.append(main)
     }
 }
 
-function completeEvent(info, main) {
-    const events = Calendar.getEvents();
-    const classList = ['completed']
+function completeEvent(info, element) {
 
-    for (let i = 0; i < events.length; i++) {
-        //events[i].setProp("classNames", classList);
+    if (!info.event.classNames.includes("completed")) {
+        info.event.setProp("classNames", info.event.classNames.concat(['completed']))
     }
+    console.log(info.event.classNames)
+    return false
 }
 
-function deleteEvent(info, main) {
-    info.event.remove();
+function deleteEvent(info, element) {
+    info.event.remove()
+    return false
+}
+
+function removeEventMenu(element) {
+    var child = element.lastElementChild
+    while (child) {
+        element.removeChild(child)
+        child = element.lastElementChild
+    }
+
+    element.parentElement.classList.remove("menuEnabled")
+    element.remove()
 }
