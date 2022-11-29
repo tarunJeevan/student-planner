8c76fbdfb22379472dbe0c29d882059cb7a8c495
Kayleb Redner <kayleb.redner@teamqualityservices.com>
2022-11-28 19:02:15 -0500

    remove console logs

--- js/signup.js
+++ js/signup.js
@@ -2,9 +2,7 @@ function validate(){
     //ensure pass and confpass are the same, return true if so, false if not
     //required tag in form takes care of the rest of the validation
     if(document.forms.signUp.pass.value!==document.forms.signUp.confPass.value){
-        console.log(document.forms.signUp.pass.value)
         return false
     }
-    console.log('called')
     return true
 }
